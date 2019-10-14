a2c5a231c7dbf99b6951773cfcac7d96c2866b86
leonardodamasceno8 <leonardodamasceno8@users>
2019-10-14 17:39:23 -0300

    dirname_config

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
+const path = require('path')
+
 const sqlite = require('sqlite')
-const dbcon = sqlite.open('jobify.sqlite', { Promise })
+const dbcon = sqlite.open(path.resolve(__dirname, 'jobify.sqlite'), { Promise })
 
 const port = process.env.PORT || 3000
 
